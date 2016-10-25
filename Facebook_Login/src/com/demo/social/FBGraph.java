7e2c96500e4642a72d027a476143be955ed5b4b7
sridhar-palakurthi <sridhar.psl.india@gmail.com>
2016-10-25 23:49:56 +0530

    FaceBook OAuth Login files changes

---
+++ Facebook_Login/src/com/demo/social/FBGraph.java
@@ -0,0 +1,61 @@
+package com.demo.social;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class FBGraph {
+	private String accessToken;
+	public static final String FB_APP_ID = "YOUR_APP_ID";
+	public static final String FB_APP_SECRET = "YOUR_APP_SECRET_KEY_HERE";
+	public static final String REDIRECT_URI = "http://localhost:8080/Facebook_Login/fbhome";
+
+	public FBGraph(String accessToken) {
+		this.accessToken = accessToken;
+	}
+
+	public String getFBGraph() {
+		String graph = null;
+		try {
+
+			String g = "https://graph.facebook.com/me?fields=id,name,email&" + accessToken;
+			URL u = new URL(g);
+			URLConnection c = u.openConnection();
+			BufferedReader in = new BufferedReader(new InputStreamReader(
+					c.getInputStream()));
+			String inputLine;
+			StringBuffer b = new StringBuffer();
+			while ((inputLine = in.readLine()) != null)
+				b.append(inputLine + "\n");
+			in.close();
+			graph = b.toString();
+			System.out.println(graph);
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new RuntimeException("ERROR in getting FB graph data. " + e);
+		}
+		return graph;
+	}
+
+	public Map<String, String> getGraphData(String fbGraph) {
+		Map<String, String> fbProfile = new HashMap<String, String>();
+		try {
+			JSONObject json = new JSONObject(fbGraph);
+			fbProfile.put("id", json.getString("id"));
+			if (json.has("name"))
+				fbProfile.put("name", json.getString("name"));
+			if (json.has("email"))
+				fbProfile.put("email", json.getString("email"));
+		} catch (JSONException e) {
+			e.printStackTrace();
+			throw new RuntimeException("ERROR in parsing FB graph data. " + e);
+		}
+		return fbProfile;
+	}
+}
